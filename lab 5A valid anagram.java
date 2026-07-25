5e09a2214aeb853248036c90c6fcb2dc5899905b
bhavanasrigr-jpg <bhavanasrigr@gmail.com>
2026-07-25 13:45:22 +0530

    Create lab 5A valid anagram.java

---
+++ lab 5A valid anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            count.merge(c, 1, Integer::sum);
+        }
+        for (char c : t.toCharArray()) {
+            count.merge(c, -1, Integer::sum);
+            if (count.get(c) == 0) {
+                count.remove(c);
+            }
+        }
+
+        return count.isEmpty();
+    }
+}
